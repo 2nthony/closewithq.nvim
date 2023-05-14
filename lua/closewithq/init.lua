4623d52fe3d68e49c5ad201af5d9d2ee9b862bb7
2nthony <ant.hu@outlook.com>
2023-05-15 02:10:21 +0800

    feat: init 2

--- lua/closewithq/init.lua
+++ lua/closewithq/init.lua
@@ -1,10 +1,28 @@
+local default_patterns = {
+  -- from lazyvim
+  "PlenaryTestPopup",
+  "help",
+  "lspinfo",
+  "man",
+  "notify",
+  "qf",
+  "spectre_panel",
+  "startuptime",
+  "tsplayground",
+  "checkhealth",
+
+  -- Overseer
+  "OverseerList",
+}
+
 local M = {}
 
 function M.setup(opts)
   opts = opts or {}
 
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = opts.pattern,
+    group = vim.api.nvim_create_augroup("closewithq", { clear = true }),
+    pattern = vim.list_extend(default_patterns, opts.patterns or {}),
     callback = function(event)
       vim.bo[event.buf].buflisted = false
       vim.keymap.set("n", "q", ":close<cr>", { buffer = event.buf, silent = true })
